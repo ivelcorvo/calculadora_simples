7bafb7f77999b8d26aeec78d7d2f85b80bf0f6a1
Levi Alves <levialvesjr@hotmail.com>
2021-03-15 17:16:44 -0300

    praticamente refeito

--- index.php
+++ index.php
@@ -1,75 +1,75 @@
 <?php
 	var_dump($_POST);	// selecionar tudo (vai estar no canto superior esquerdo)
 
-	$res = 0;
+$resultado=0;
 
-	if(isset($_POST['op']) && count($_POST)){
-		switch($_POST['op']){
-			case 'som':
-				$res = $_POST['n1'] + $_POST['n2'];
+	if(isset($_POST["op"]) && count($_POST)){
+		switch($_POST["op"]){
+			case "som":
+				$resultado = $_POST["n1"] + $_POST["n2"];
 				break;
-			case 'sub':
-				$res = $_POST['n1'] - $_POST['n2'];
+			case "sub":
+				$resultado = $_POST["n1"] - $_POST["n2"];
 				break;
-			case 'mul':
-				$res = $_POST['n1'] * $_POST['n2'];
+			case "mul":
+				$resultado = $_POST["n1"] * $_POST["n2"];
 				break;
-			case 'div':
-				if($_POST['n2'] == 0){
-					echo'<p class="alerta"> Não é possível dividir por zero </p>';
-				}else
-					$res = $_POST['n1'] / $_POST['n2'];
+			case "div":
+				if($_POST["n2"] == 0){
+					$resultado="Não é possível dividir por zero.";
+				}else{
+					$resultado = $_POST["n1"] / $_POST["n2"];
+				}				
 				break;
-			default:
-				echo'Erro';
 		}
-	}else
-		echo"Não há registro";
+	}else{
+		echo"não há registro";
+	}
 ?>
 
 
 <!DOCTYPE html>
-<html lang = 'ptbr'>
-	<head>
-		<title>php</title>
-		<link rel='stylesheet' href='_css/estilo.css'>
+<html lang = "pt-br">
+	<head>		
 		<meta charset="utf-8">
+		<meta name="author" content="Levi Alves">
+		<meta name="description" content="calculadora simples">
+		<meta name="robots" content="index, nofolow">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>php</title>
+		<link rel="stylesheet" type="text/css" href="_css/estilo.css">
 	</head>
 	<body>
-		<div class='header'>
+		<header class="conteiner">
 			<h1> CALCULADORA </h1>
-		</div>
+		</header>
 
-		<div class='conteiner'>
-			<div class='principal'>
-				<form method="post">
-					<div class="fields">
-						<div class="input-field">
-							<input id="n1" type="number" name="n1">
-							<label for="n1">número 1</label>
-						</div>
-						<div class="input-field">
-							<input id="n2" type="number" name="n2">
-							<label for="n2">número 2</label>
-						</div>
-						<div class="btn-field">
-							<button type="submit" name="op" value="som"
-									class="btn">SOMA</button>
-							<button type="submit" name="op" value="sub"
-									class="btn">SUBTRAÇÃO</button>
-							<button type="submit" name="op" value="div"
-									class="btn">DIVISÃO</button>
-							<button type="submit" name="op" value="mul"
-									class="btn">MULTIPLICAÇÃO</button>
-						</div>
-						<div class="output-field">
-							Resultado: <?php echo $res ?>
-						</div>
-					</div>					
-				</form>
-			</div>
-		</div>
-		<div class='footer'>
+		<section class="principal conteiner">
+			<form method="post">
+				<fieldset class="flex">
+					<div class="field">
+						<label for="n1">1º Número:</label>						
+						<input type="number" name="n1" id="n1" class="campo" required autofocus><br><br>
+						<label for="n2">2º Número:</label>
+						<input type="number" name="n2" id="n2" class="campo" required>
+					</div>
+					<div class="field flex">
+						<button type="submit" class="btn" name="op" value="som">Somar</button>
+						<button type="submit" class="btn" name="op" value="sub">Subtração</button>
+						<button type="submit" class="btn" name="op" value="mul">Multiplicação</button>
+						<button type="submit" class="btn" name="op" value="div">Divisão</button>
+					</div>
+					<div class="field">
+						<?php 
+							echo"<p>".$resultado."</p>";
+						?>
+					</div>
+				</fieldset>				
+			</form>
+		</section>
+
+		<div class="footer">
 		</div>	
 	</body>
 </html>
